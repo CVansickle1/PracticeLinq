dafa76148058fda7c343e4c7db01907984d1d37a
Cody <Cojo341@gmail.com>
2022-08-26 12:25:58 -0400

    Inital Commmit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+var videoGames = new List<string>() {"Fall Guys", "Morturary Assistant", "Apex", "Overwatch", "Vampire Surviors", "Powerwashing Simulator"};
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(game => Console.WriteLine(game));
+
